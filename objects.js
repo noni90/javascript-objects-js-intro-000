efbe316da295c09920d4fd3b06d3c2c42ad95d54
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-21 13:31:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,5 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle)
 }
 function removeFromPlaylist(playlist, artistName)
 {
-  Delete playlist[artistName];
+  delete playlist[artistName];
+  return playlist;
 }
