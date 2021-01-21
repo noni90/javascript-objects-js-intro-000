a1fe0ba3bc372e77a767e6dd3a4562f3f3ce4584
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-21 13:30:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist ={'Foo Fighters': 'Everlong',
 function updatePlaylist(playlist, artistName, songTitle)
 
 {
-  playlist.artistName=songTitle;
+  playlist[artistName]=songTitle;
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName)
